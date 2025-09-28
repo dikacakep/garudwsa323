fcbd3b90f6ee819d9aae873161faae5c0cff071b
iRexus <101460160+dikacakep@users.noreply.github.com>
2025-09-29 04:10:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    return res.status(200).send('Garuda webhook online');
-  }
-  res.setHeader('Allow', ['GET']);
-  res.status(405).end(`Method ${req.method} Not Allowed`);
-}
+Garuda Online
